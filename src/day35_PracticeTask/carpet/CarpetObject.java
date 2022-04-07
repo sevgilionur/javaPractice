53afba1c60c6e989a3a9310caf46c29fdbeccb28
onur <sevgilionur25@hotmail.com>
2022-04-08 02:11:13 +0300

    .gitignore

---
+++ src/day35_PracticeTask/carpet/CarpetObject.java
@@ -0,0 +1,15 @@
+package day35_PracticeTask.carpet;
+
+public class CarpetObject {
+
+    public static void main(String[] args) {
+
+        Carpet carpet1 = new Carpet(10,20,5,true);
+
+        System.out.println(carpet1);
+
+
+
+
+    }
+}

4eda43e59cc18fb8a06e514152e7c9e523041157
onur <sevgilionur25@hotmail.com>
2022-04-08 02:58:06 +0300

    Carpet Task

---
+++ src/day35_PracticeTask/item/ItemObject.java
@@ -0,0 +1,12 @@
+package day35_PracticeTask.item;
+
+public class ItemObject {
+
+    public static void main(String[] args) {
+
+        Item item1 = new Item("toilet paper", 10, 3);
+
+        System.out.println(item1);
+
+    }
+}

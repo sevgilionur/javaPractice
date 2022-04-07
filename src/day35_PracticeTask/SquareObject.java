6a8dc7b6af6ff2caf874f05b4682d2d9322a0d0a
onur <sevgilionur25@hotmail.com>
2022-04-08 01:40:17 +0300

    .gitignore

---
+++ src/day35_PracticeTask/SquareObject.java
@@ -0,0 +1,23 @@
+package day35_PracticeTask;
+
+public class SquareObject {
+
+    public static void main(String[] args) {
+
+        Square square1 = new Square(10.2);
+
+        System.out.println(square1);
+
+        System.out.println(square1.calcArea());
+        System.out.println(square1.calcPerimeter());
+
+
+
+
+
+
+
+
+
+    }
+}

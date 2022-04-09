35b14c63cc4aee9d00048d7ed0735a20c9a10797
onur <sevgilionur25@hotmail.com>
2022-04-09 17:19:17 +0300

    Task Solutions

---
+++ src/day35_PracticeTask/credentials/CredentialsObject.java
@@ -0,0 +1,13 @@
+package day35_PracticeTask.credentials;
+
+public class CredentialsObject {
+
+    public static void main(String[] args) {
+
+        Credentials credentials = new Credentials("Onur", "1Abcdefg@");
+
+        System.out.println(credentials);
+
+
+    }
+}
